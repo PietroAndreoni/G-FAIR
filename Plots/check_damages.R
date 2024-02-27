f16b99b448e4994632024ce7ab1770b66ea8bcc4
Pietro <pietro.andreoni@eiee.org>
2024-02-26 19:32:15 -0600

    + on plots for Keith damage function

--- Plots/check_damages.R
+++ Plots/check_damages.R
@@ -1,11 +1,11 @@
-alpha <- 0.23 #[%GDP/K^2]
+alpha <- 0.595 #[%GDP/K^2] Howard/Stern
 sens <- 0.84 #[0.84 K/(W*m^2)]
 direct_costs <- 0.1 #[%GDP/(W*m^2)]
 power <- 2 #exponent of direct costs
 Cmax <- 4#
 cost <- 0.2
 
-study <- cross_join(data.frame(C=seq(0,Cmax,0.5)),
+study <- cross_join(data.frame(C=seq(0.5,Cmax,0.5)),
                     data.frame(g=seq(0,1,0.1))) %>%
   cross_join(data.frame(theta=seq(0,60,by=10)*pi/180)) %>%
   mutate(direct = direct_costs*(g*C)^power,
@@ -19,12 +19,13 @@ study <- cross_join(data.frame(C=seq(0,Cmax,0.5)),
   mutate(valuenorm=ifelse(name!="efficacy",value/value[name=="total" & g==0],value))
 
 ggplot(study %>% 
-         filter(theta==30*pi/180 & C %in% c(1,4) & name!="efficacy")) +
+         filter(theta %in% c(10*pi/180 ,30*pi/180 ) & C %in% c(1,4) & name!="efficacy")) +
   geom_line(aes(x=g,
                 y=valuenorm,
                 color=name,
-                linetype=as.factor(C) ),
-            linewidth=1)
+                linetype=as.factor(theta*180/pi) ),
+            linewidth=1) +
+  facet_wrap(C~.,)
 
 ggplot(study %>% 
          filter(C==1 & name=="efficacy")) +
