43e56de187549ee28dbced80cab7e1f411b363b4
Pietro <pietro.andreoni@cmcc.it>
2025-03-17 18:55:09 +0100

    Update experiment script for replication

--- run_experiments.bat
+++ run_experiments.bat
@@ -2,13 +2,13 @@
 setlocal enabledelayedexpansion
 
 REM Define your variable (e.g., a list of filenames)
-set "gas=co2 ch4 n2o"
+set "gas=co2 ch4"
 
 REM Loop over the variable and execute a command (e.g., printing each filename)
 for %%i in (%gas%) do (
     echo Processing file: %%i
-    gams FAIR.gms --initial_conditions=historical_run --experiment_ghg=pulse --gas=%%i --sai=1
-    gams FAIR.gms --initial_conditions=historical_run --experiment_ghg=pulse --gas=%%i 
+    gams FAIR.gms --experiment_ghg=pulse --gas=%%i --sai=1
+    gams FAIR.gms --experiment_ghg=pulse --gas=%%i 
 )
 
 endlocal
